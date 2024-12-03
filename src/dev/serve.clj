7ac69d33183a6fc37b8c5172e89d7dbcee8df634
Graham Carlyle <683360+grahamcarlyle@users.noreply.github.com>
2024-12-03 15:13:30 +0000

    serve alias for viewing static site locally

---
+++ src/dev/serve.clj
@@ -0,0 +1,12 @@
+(ns serve
+  (:require
+    [ring.adapter.jetty :as jetty]
+    [ring.middleware.file :as file-middleware]))
+
+(defn serve-site [{:keys [dir]}]
+  (let [no-dynamic-content-handler {}
+        handler (file-middleware/wrap-file no-dynamic-content-handler dir)
+        server (jetty/run-jetty handler {:port 8080 :join? false})]
+    (println (format "serving %s from %s" (.getURI server) dir))
+    (.join server)
+    ))
