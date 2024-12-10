5346303be7542ffa2d38f03319b172bc267a05ac
Graham Carlyle <683360+grahamcarlyle@users.noreply.github.com>
2024-12-10 12:50:38 +0000

    render post as a template and an optional containing template

--- src/main/com/grahamcarlyle/blog/posts.clj
+++ src/main/com/grahamcarlyle/blog/posts.clj
@@ -35,21 +35,38 @@
 (defn parse [content]
   (let [split-content (split-front-matter content)]
     {:meta   (some-> split-content :front-matter-yaml (yaml/parse-string))
-     :hiccup (-> split-content :markdown (parse-markdown))}))
+     :content (-> split-content :markdown (parse-markdown))}))
 
-(defn eval-string [form-str context]
-  (sci/eval-string form-str {:bindings context}))
+(defn eval-string [form-str namespaces]
+  (sci/eval-string form-str {:namespaces namespaces}))
 
 (defn eval-template
-  ([template]
-   (eval-template template nil))
-  ([template context]
-   (walk/postwalk
-     (fn [x]
-       (if (and (vector? x)
-                (= 2 (count x))
-                (= ::template (first x))
-                (string? (second x)))
-         (str (eval-string (second x) context))
-         x))
-     template)))
+  [template & {:keys [namespaces raw?]}]
+  (walk/postwalk
+    (fn [x]
+      (if (and (vector? x)
+               (= 2 (count x))
+               (= ::template (first x))
+               (string? (second x)))
+        (cond-> (eval-string (second x) namespaces)
+                (not raw?) (str))
+        x))
+    template))
+
+(def undecorated-content-template [::template "page/content"])
+
+(defn render
+  ([post-markdown]
+   (render post-markdown nil))
+  ([post-markdown context]
+   (let [{:keys [meta content]} (parse post-markdown)
+         meta-mapping (update-keys meta #(symbol (name %)))
+         content (eval-template content {:namespaces {'user meta-mapping}})
+         template-name (-> meta :template (keyword))
+         template (or (and template-name
+                           (or (-> context :templates (get template-name))
+                               (throw (ex-info "Unknown template" {:template-name template-name}))))
+                      undecorated-content-template)
+         template-mapping (assoc {'post {'content content}}
+                            'post.meta meta-mapping)]
+     (eval-template template {:namespaces template-mapping :raw? true}))))
--- src/test/com/grahamcarlyle/blog/posts_test.clj
+++ src/test/com/grahamcarlyle/blog/posts_test.clj
@@ -5,14 +5,14 @@
 
 (deftest parse-posts-test
   (testing "Parse simple markdown"
-    (is (= {:meta   nil
-            :hiccup [:div [:p "This is a post"]]}
+    (is (= {:meta    nil
+            :content [:div [:p "This is a post"]]}
            (posts/parse "This is a post"))))
 
   (testing "Parse markdown with yaml front matter metadata"
-    (is (= {:meta   {:title "First post"
-                     :tags ["foo" "bar"]}
-            :hiccup [:div [:p "This is a post"]]}
+    (is (= {:meta    {:title "First post"
+                      :tags  ["foo" "bar"]}
+            :content [:div [:p "This is a post"]]}
            (posts/parse
              "---
 title: First post
@@ -24,8 +24,8 @@ This is a post"))))
 
   (testing "Parse markdown with liquid like templating"
     (is (= [:div [:p "2 + 1 = " [::posts/template " (+ 2 1) "]]]
-           (:hiccup (posts/parse
-                      "2 + 1 = {{ (+ 2 1) }}"))))))
+           (:content (posts/parse
+                       "2 + 1 = {{ (+ 2 1) }}"))))))
 
 (deftest eval-template-test
   (testing "simple expression evaluation"
@@ -33,4 +33,30 @@ This is a post"))))
   (testing "nested expression evaluation"
     (is (= [:div "3"] (posts/eval-template [:div [::posts/template "(+ 1 2)"]]))))
   (testing "reference variable in context"
-    (is (= "4" (posts/eval-template [::posts/template "(+ 1 a)"] {'a 3})))))
+    (is (= "4" (posts/eval-template [::posts/template "(+ 1 a)"] {:namespaces {'user {'a 3}}}))))
+  (testing "reference variable in other namespace context"
+    (is (= "4" (posts/eval-template [::posts/template "(+ 1 ns1/a)"] {:namespaces {'ns1 {'a 3}}}))))
+  (testing " "
+    (is (= 4 (posts/eval-template [::posts/template "(+ 1 3)"] {:raw? true})))))
+
+(deftest render-post-test
+  (testing "a templated markdown post referencing the front matter meta variables"
+    (is (= [:html
+            [:head [:title "A title"]]
+            [:body
+             [:div
+              [:h1 {:id "title"} "A title"]
+              [:p "This is a " "short" " post."]]]]
+           (posts/render "---
+template: simple
+title: A title
+adjective: short
+---
+# {{ title }}
+This is a {{ adjective }} post."
+             {:templates {:simple [:html
+                                   [:head [:title [::posts/template "post.meta/title"]]]
+                                   [:body
+                                    [::posts/template "post/content"]]]}}))))
+  (testing "TODO no template")
+  (testing "TODO Unknown template"))
