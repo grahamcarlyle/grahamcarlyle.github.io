8ffe6fec3a118eb114244d742624e2f97d530318
Graham Carlyle <683360+grahamcarlyle@users.noreply.github.com>
2024-12-13 12:12:06 +0000

    fix generate by migrating to render

--- src/main/com/grahamcarlyle/blog/build.clj
+++ src/main/com/grahamcarlyle/blog/build.clj
@@ -31,14 +31,12 @@
 (defn generate [{:keys [output-dir]}]
   (fs/delete-tree output-dir)
   (fs/create-dirs output-dir)
-  (let [result (posts/parse (slurp (io/file "posts" "first-scrappy.md")))
-        mapping (merge {::page/content (:hiccup result)}
-                       (qualify-plain-keyword-keys (:meta result) (namespace ::page/placeholder)))]
+  (let [result (posts/render (slurp (io/file "posts" "first-scrappy.md")))]
     (spit (io/file output-dir "index.html")
-          (h/html (h/raw "<!DOCTYPE html>")
-                  (substitute post-template mapping)))))
+          (h/html (h/raw "<!DOCTYPE html>") result))))
 
 (comment
   (def output-dir "generated-output")
   (fs/delete-tree output-dir)
-  (generate {:output-dir output-dir}))
+  (generate {:output-dir output-dir})
+  ,)
