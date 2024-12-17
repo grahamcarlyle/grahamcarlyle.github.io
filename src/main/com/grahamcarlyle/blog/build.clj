fa79bcc2107aec324d7109bf2393b6991089f95b
Graham Carlyle <683360+grahamcarlyle@users.noreply.github.com>
2024-12-17 20:52:56 +0000

    Generate pages for all the posts

--- src/main/com/grahamcarlyle/blog/build.clj
+++ src/main/com/grahamcarlyle/blog/build.clj
@@ -9,12 +9,15 @@
 (defn generate [{:keys [output-dir]}]
   (fs/delete-tree output-dir)
   (fs/create-dirs output-dir)
-  (let [result (posts/render (slurp (io/file "posts" "first-scrappy.md")))]
-    (spit (io/file output-dir "index.html")
-          (h/html (h/raw "<!DOCTYPE html>") result))))
+  (doseq [post-file (fs/glob "posts" "*.md")]
+    (let [result (posts/render (slurp (fs/file post-file)))]
+      (spit (io/file output-dir (str (fs/strip-ext (last (fs/components post-file))) ".html"))
+            (h/html (h/raw "<!DOCTYPE html>") result)))))
 
 (comment
   (def output-dir "generated-output")
   (fs/delete-tree output-dir)
   (generate {:output-dir output-dir})
+
+  (fs/strip-ext (last (fs/components (first (fs/glob "posts" "*.md")))))
   ,)
